941a5cf0307473ed474aca2bbcf86afb4c096252
Leo Booth <leobooth@users.noreply.github.com>
2022-07-03 07:36:15 +0000

    added a 'How To' section that displays if the main program is run

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) throws Exception {
-    System.out.println("tests are located in Unit Tests menu");
+    System.out.println("");
+    System.out.println("How To Use This Tutorial");
+    System.out.println("========================");
+    System.out.println("Click the 'check mark' icon to view the Unit Tests section.");
+    System.out.println("Click the 'Setup' button. In the 'Imports' section, you will see that Replit tests use the JUnit test library. Along with JUnit, the tutorial also imports Selenium and Chromium.");
+    System.out.println("Under the 'Advanced' section, you will see subsections for 'Initialization', 'Setup', and 'Teardown'.");
+    System.out.println("Use the 'Initialization' section to create any global variables needed by your tests.");
+    System.out.println("Use the 'Setup' and 'Teardown' sections to create JUnit Before and After methods.");
+    System.out.println("Use the 'Run Tests' button to run your tests.");
   } 
 }
